518c9a465f4b94afcd58db9235d7b6d4b9a11f02
Partha28102021 <ujsanofficial@gmail.com>
2025-03-03 22:33:33 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,24 @@
+const config = {
+    multiplier: 9        ,
+    candles: 1500                   ,
+    threshold: 30000000           ,
+    interval: "15"
+};
+
+
+
+
+/*
+
+-------- FOR INTERAVALS --------
+
+1 minute = "1"
+30 minutes = "30"
+1 hour = "60"
+4 hours = "240" 
+1 day = "D"
+1 week = "W"
+
+
+Please keep the double-quotes, as it is a part of the syntax
+*/
