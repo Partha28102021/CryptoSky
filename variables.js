d8e35fbdc95f56cd63e13d2b8c0b195e2484d13f
Partha28102021 <ujsanofficial@gmail.com>
2025-03-08 02:02:01 +0530

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,8 +1,8 @@
 const config = {
-    multiplier: 1    ,
+    multiplier: 9   ,
     candles: 1500                   ,
-    threshold: 300,
-    interval: "1"
+    threshold: 30000000
+    interval: "15"
 };
 
 
